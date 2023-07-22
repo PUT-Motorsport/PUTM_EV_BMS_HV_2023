83aba4bfe72286830437b4c2bf17db051afe76c3
SzalonyJohny <janwg84@gmail.com>
2023-07-22 18:52:45 +0200

    Added can start

---
+++ firmware/Core/Inc/can_utils.hpp
@@ -0,0 +1,16 @@
+/*
+ * can_utils.hpp
+ *
+ *  Created on: Jul 22, 2023
+ *      Author: jan
+ */
+
+#ifndef INC_CAN_UTILS_HPP_
+#define INC_CAN_UTILS_HPP_
+
+#include "main.h"
+
+bool startCan(FDCAN_HandleTypeDef &hfdcan);
+
+
+#endif /* INC_CAN_UTILS_HPP_ */
--- firmware/Core/Src/Tasks/ChargerCANManager.cpp
+++ firmware/Core/Src/Tasks/ChargerCANManager.cpp
@@ -8,63 +8,34 @@
 #include "app_freertos.h"
 #include "PerypherialManagers/Charger.hpp"
 #include "main.h"
+#include "can_utils.hpp"
 
 extern FDCAN_HandleTypeDef hfdcan2;
-void startCanCharger();
 
 void vChargerCANManagerTask(void *argument)
 {
-	startCanCharger();
-	// FIXME to debug CAN communication
-	bool charger_connected{false};
+	startCan(hfdcan2);
 	bool charging_enable{false};
 	float charge_voltage = 135.0f * 4.15f;
 	float charge_current = 2.0f;
-	ChargerCanRxMessageHandler charger_rx;
+	ChargerCanRxMessageHandler charger_rx{};
 
 	while (true)
 	{
-		osDelay(100);
-		charger_rx.update();
-		ChargerCanTxMessage frame{charge_voltage, charge_current, charging_enable};
-		frame.send();
+		bool charger_connected = not HAL_GPIO_ReadPin(CHARGER_DETECT_GPIO_Port, CHARGER_DETECT_Pin);
+		if(charger_connected){
+			osDelay(100);
+			charger_rx.update();
+			ChargerCanTxMessage frame{charge_voltage, charge_current, charging_enable};
+			frame.send();
+		}
+		else {
+			osDelay(3000);
+		}
+
 	}
 }
 
 
 
-void startCanCharger(){
 
-	constexpr static FDCAN_FilterTypeDef sFilterConfig = []
-		{
-			FDCAN_FilterTypeDef temp{};
-			temp.IdType = FDCAN_STANDARD_ID;
-			temp.FilterIndex = 0;
-			temp.FilterType = FDCAN_FILTER_MASK;
-			temp.FilterConfig = FDCAN_FILTER_TO_RXFIFO0;
-			temp.FilterID1 = 0;
-			temp.FilterID2 = 0;
-			return temp;
-		}();
-
-		const auto can_notification_status =
-			HAL_FDCAN_ActivateNotification(&hfdcan2, FDCAN_IT_RX_FIFO0_NEW_MESSAGE, 0);
-		if (HAL_OK != can_notification_status)
-		{
-			Error_Handler();
-		}
-
-		const auto can_filter_config_status =
-			HAL_FDCAN_ConfigFilter(&hfdcan2, const_cast<FDCAN_FilterTypeDef *>(&sFilterConfig));
-
-		if (HAL_OK != can_filter_config_status)
-		{
-			Error_Handler();
-		}
-
-		if (HAL_FDCAN_Start(&hfdcan2) != HAL_OK)
-		{
-			Error_Handler();
-		}
-
-}
