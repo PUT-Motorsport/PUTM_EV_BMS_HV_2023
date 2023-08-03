17671500e2208057258ba26437900eac4a26d593
SzalonyJohny <janwg84@gmail.com>
2023-08-03 17:11:56 +0200

    Can car sending cell voltage data

--- firmware/Core/Inc/stackData.hpp
+++ firmware/Core/Inc/stackData.hpp
@@ -8,45 +8,58 @@
 #ifndef STACKDATA_HPP_
 #define STACKDATA_HPP_
 
-#include <array.h>
+#include <array>
 #include <atomic>
 
 constexpr static size_t bat_stack_size = 135;
-// FIXME: do sprawdzenia ile temperatur
-constexpr static size_t temp_stack_size = 65; // 3 * 5
+constexpr static size_t temp_stack_size = 65; // 3 * 5 FIXME: do sprawdzenia ile temperatur
 
-class FullStackData
+struct FullStackData
 {
 	struct LTCData
 	{
-		static std::array<std::atomic<float>, bat_stack_size> volt;
-		static std::array<std::atomic<bool>, bat_stack_size> balance;
-		static std::array<std::atomic<float>, temp_stack_size> temp;
-		static float bat_volt;
-	};
+		std::array<std::atomic<float>, bat_stack_size> volt;
+		std::array<std::atomic<bool>, bat_stack_size> balance;
+		std::array<std::atomic<float>, temp_stack_size> temp;
+		std::atomic<float> min_temp;
+		std::atomic<float> max_temp;
+		std::atomic<float> bat_volt;
+		std::atomic<float> soc;
+	} ltcData;
+
 	struct ExternalData
 	{
-		static std::atomic<float> car_volt;
-		static std::atomic<float> acu_volt;
-		static std::atomic<float> acu_curr;
-	};
+		std::atomic<float> car_volt;
+		std::atomic<float> acu_volt;
+		std::atomic<float> acu_curr;
+	} externalData;
+
 	struct Other
 	{
-		static std::atomic<int> imd_state;
-		//
-	};
+		std::atomic<int> imd_state;
+	} other;
+
 	struct Air
 	{
-		static std::atomic<bool> pre_state;
-		static std::atomic<bool> p_state;
-		static std::atomic<bool> m_state;
-	};
+		std::atomic<bool> pre_state;
+		std::atomic<bool> p_state;
+		std::atomic<bool> m_state;
+	} air;
 };
 
-inline FullStackData &get_FullStackData()
+class FullStackDataInstance
 {
-	static FullStackData FullStackData{};
-	return FullStackData;
-}
+public:
+	static FullStackData &get()
+	{
+		static FullStackData instance{};
+		return instance;
+	}
+
+	static const FullStackData &getConst()
+	{
+		return get();
+	}
+};
 
 #endif /* STACKDATA_HPP_ */
--- firmware/Core/Src/Tasks/CarCANManager.cpp
+++ firmware/Core/Src/Tasks/CarCANManager.cpp
@@ -5,24 +5,80 @@
  *      Author: jan
  */
 
-#include "PerypherialManagers/CanUtils.hpp"
 #define PUTM_USE_CAN_FD
 #include "PUTM_EV_CAN_LIBRARY/lib/can_interface.hpp"
+#include "PerypherialManagers/CanUtils.hpp"
 #include "app_freertos.h"
 #include "fdcan.h"
 #include "main.h"
+#include "stackData.hpp"
 
 static FDCAN_HandleTypeDef &hfdcan = hfdcan3;
+size_t send_voltages_message(size_t cell_index);
 
 void vCarCANManagerTask(void *argument)
 {
+	size_t cell_send_index{};
 	startCan(hfdcan);
 	while (true)
 	{
-		osDelay(5);
+		osDelay(10);
 		while (get_can_fifo_message_count(hfdcan))
 		{
 			PUTM_CAN::can.parse_message(PUTM_CAN::Can_rx_message(hfdcan));
 		}
+
+		const FullStackData &fsd = FullStackDataInstance::getConst();
+
+		PUTM_CAN::BMS_HV_main main_frame = {
+			.voltage_sum = static_cast<uint16_t>(fsd.ltcData.bat_volt),
+			.current = static_cast<int16_t>(fsd.externalData.acu_curr * 100.0f),
+			.temp_max = static_cast<uint8_t>(fsd.ltcData.max_temp),
+			.temp_avg = static_cast<uint8_t>(fsd.ltcData.min_temp),
+			.soc = static_cast<uint16_t>(fsd.ltcData.soc * 1000)};
+
+		auto status = PUTM_CAN::Can_tx_message(main_frame, PUTM_CAN::can_tx_header_BMS_HV_MAIN).send(hfdcan);
+
+		if (status not_eq HAL_OK)
+		{
+			// FIXME Handle error
+		}
+
+		cell_send_index = send_voltages_message(cell_send_index);
+	}
+}
+
+
+size_t send_voltages_message(size_t cell_index)
+{
+	const FullStackData &fsd = FullStackDataInstance::getConst();
+
+	auto voltage_to_can = [](float voltage) -> uint16_t
+	{
+		return static_cast<uint16_t>(voltage * 1'000 - 2'850);
+	};
+
+	const PUTM_CAN::BMS_HV_cell_voltages frame = {
+		.cell_start = static_cast<uint8_t>(cell_index),
+		.v_cell_start = voltage_to_can(fsd.ltcData.volt.at(cell_index)),
+		.v_cell_start_plus_1 = voltage_to_can(fsd.ltcData.volt.at(cell_index + 1)),
+		.v_cell_start_plus_2 = voltage_to_can(fsd.ltcData.volt.at(cell_index + 2)),
+		.v_cell_start_plus_3 = voltage_to_can(fsd.ltcData.volt.at(cell_index + 3)),
+		.cell_end = static_cast<uint8_t>(cell_index + 3)
+	};
+
+	auto status = PUTM_CAN::Can_tx_message(frame, PUTM_CAN::can_tx_header_BMS_HV_cell_voltages).send(hfdcan);
+	if (status not_eq HAL_OK)
+	{
+		// FIXME Handle error
+	}
+
+	if (cell_index + 4 < fsd.ltcData.volt.size())
+	{
+		return cell_index + 4;
+	}
+	else
+	{
+		return 0;
 	}
 }
