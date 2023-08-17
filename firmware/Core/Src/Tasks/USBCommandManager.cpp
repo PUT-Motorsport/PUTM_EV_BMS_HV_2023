2350d5208f439e369e203cdb07796397feff0af0
KozAAAAA <mateusz-koza@outlook.com>
2023-08-17 20:22:10 +0200

    Add print to the console

--- firmware/Core/Src/Tasks/USBCommandManager.cpp
+++ firmware/Core/Src/Tasks/USBCommandManager.cpp
@@ -9,12 +9,32 @@
 
 #include <app_freertos.h>
 #include <StackData.hpp>
+#include <usb_device.h>
+#include <usbd_cdc_if.h>
+#include <string.h>
+
+
+// https://community.st.com/t5/embedded-software-mcus/way-to-be-notified-on-cdc-transmit-complete/td-p/412336
+
+static const char *data = "Hello World!\r\n";
 
 void vUSBCommandManagerTask(void *argument)
 {
+
 	while(true)
 	{
-		osDelay(100);
+    // Pack data into 64 byte buffer // TODO: Check if it fits our data
+    // Transmit data
+
+    // To imlement this in the INTERRUPT context we have to mess up with the HAL libraries
+		// This only queues the data:
+    uint8_t status = CDC_Transmit_FS((uint8_t*)data, strlen(data));
+
+    // This is a callback that has to be implemented in usbd_cdc_if.c
+    // CDC_Receive_FS();
+
+
+    osDelay(1000);
 	}
 }
 
