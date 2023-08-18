2d158472175e95d6555901858cd35c337ef877cb
KozAAAAA <mateusz-koza@outlook.com>
2023-08-18 16:17:52 +0200

    Create JSON, Send JSON via USB

--- firmware/Core/Src/Tasks/USBCommandManager.cpp
+++ firmware/Core/Src/Tasks/USBCommandManager.cpp
@@ -13,28 +13,21 @@
 #include <usbd_cdc_if.h>
 #include <string.h>
 
+#include "embedded_json/include/embedded_json.hpp"
 
 // https://community.st.com/t5/embedded-software-mcus/way-to-be-notified-on-cdc-transmit-complete/td-p/412336
 
-static const char *data = "Hello World!\r\n";
-
 void vUSBCommandManagerTask(void *argument)
 {
+	embeddedjson::Json<3000> json;
+	json.add("hejka", 12);
+
+	auto [array, size] = json.get_as_c_array();
 
 	while(true)
 	{
-    // Pack data into 64 byte buffer // TODO: Check if it fits our data
-    // Transmit data
-
-    // To imlement this in the INTERRUPT context we have to mess up with the HAL libraries
-		// This only queues the data:
-    uint8_t status = CDC_Transmit_FS((uint8_t*)data, strlen(data));
-
-    // This is a callback that has to be implemented in usbd_cdc_if.c
-    // CDC_Receive_FS();
-
-
-    osDelay(1000);
+		uint8_t status = CDC_Transmit_FS((uint8_t*)array, size);
+		osDelay(1000);
 	}
 }
 
