5bcb3fb9cbf1b5d1aeadfa52932475ec814324d7
Karanami <piotrlesicki04@gmail.com>
2025-01-19 13:17:00 +0100

    docs matlab update

--- documentation/matlab/main.m
+++ documentation/matlab/main.m
@@ -3,21 +3,24 @@ clear;
 
 [freq, gain, phase] = csvimport('../sdc_data/bode_wave_cut.csv', 'columns', { 'Freq(Hz)', 'Gain(dB)', 'Phase'});
 
-gain = -1 * gain
-phase = -1 * phase
+gain = -1 * gain;
+gain = 10 .^ (gain ./ 20)
+phase = -1 * phase;
+freq = freq * 2 * pi;
 
 response = gain .* exp(1i * phase * pi / 180); % To calculate frequency response
-frdsys = frd(response, freq) % To create a frequency response data (FRD) model sys using the complex response and frequency values
-gfr = idfrd(frdsys) % To create an identified FRD model gfr based on the FRD model sys
+% gfr = idfrd(permute(response,[2 3 1]), freq, 0, 'FrequencyUnit', 'Hz');
+frdsys = frd(response, freq); % To create a frequency response data (FRD) model sys using the complex response and frequency values
+gfr = idfrd(frdsys); % To create an identified FRD model gfr based on the FRD model sys
 
 % bode(frdsys);
 % 
-% nyquist(gfr);
+% nyquist(gfr);]
 
-tf2 = tfest(gfr, 2, 0)  % Estimate a transfer function model tf2 from gf
+tf2 = tfest(gfr, 2, 2)  % Estimate a transfer function model tf2 from gf
 
-figure
+% figure
 step(tf2);   % Plot the step response of tf2
 figure
-bode(tf2);
+bode(tf2, frdsys);
 
